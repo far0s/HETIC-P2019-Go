d502fc7f3020c6f54413ac53d63f64e36a5d57d5
Sebastien Dancer-Michel <s.dancermichel@gmail.com>
2015-06-19 15:28:29 +0200

    last page click now removes/add the menu
    transition is WIP though

--- js/index.js
+++ js/index.js
@@ -12,16 +12,16 @@ $(window).load(function() {
     var page_num = pages.index(page) + 1;
     if (page_num % 2 === 0) {
       $('#play').click(function() {
-        console.log('play !');
+        // console.log('play !');
         $('book').addClass('swiped');
       });
       page.removeClass('flipped');
       page.prev().removeClass('flipped');
-      console.log('flip back !');
+      // console.log('flip back !');
     } else {
       page.addClass('flipped');
       page.next().addClass('flipped');
-      console.log('flip !');
+     // console.log('flip !');
     }
   });
   $('.book').addClass('bound');
